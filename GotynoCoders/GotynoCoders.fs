a3c6368d19f8b8edfa0f364a48b80e64f3d67eb8
Rickard Andersson <gonz@severnatazvezda.com>
2021-01-05 19:03:51 +0200

    add `encodeList`
    Takes an encoder, encodes a list of that type.

--- GotynoCoders/GotynoCoders.fs
+++ GotynoCoders/GotynoCoders.fs
@@ -2,12 +2,17 @@ module GotynoCoders
 
 open Thoth.Json.Net
 
+let encodeList encoder value =
+    value
+    |> List.map encoder
+    |> Encode.list
+
 let decodeLiteral decoder literal: Decoder<'a> =
     let decodeLiteral =
         function
         | l when l = literal -> Decode.succeed literal
         | other -> Decode.fail (sprintf "Expected literal value %A, got: %A" literal other)
-
+        
     decoder |> Decode.andThen decodeLiteral
 
 let decodeLiteralString s: Decoder<string> =
