5a091cdff897537c59c0be8bcfcde5177882dc53
Rickard Andersson <gonz@severnatazvezda.com>
2021-01-07 20:42:43 +0200

    add basic facilities for decoding tag fields
    Also a first draft of `decodeWithTypeTag`.
    Should be a decoder that allows to specify an associative
    list of tag values & decoders.

--- GotynoCoders/GotynoCoders.fs
+++ GotynoCoders/GotynoCoders.fs
@@ -17,3 +17,29 @@ let decodeLiteralString s: Decoder<string> = decodeLiteral Decode.string s
 let decodeLiteralUnsignedInteger u: Decoder<uint64> = decodeLiteral Decode.uint64 u
 
 let decodeLiteralSignedInteger i: Decoder<int64> = decodeLiteral Decode.int64 i
+
+let decodeTypeTag tagField =
+    Decode.object (fun get -> get.Required.Field tagField Decode.string)
+
+type DecoderPair<'a> = string * Decoder<'a>
+
+type DecoderSpecification<'a> = DecoderPair<'a> array
+
+let decodeWithTypeTag tagField (specification: DecoderSpecification<'a>): Decoder<'a> =
+    let applyMatchingDecoder (foundTag: string) =
+        match Array.tryFind (fun (t, _) -> t = foundTag) specification with
+        | Some (_, decoder) -> decoder
+        | None ->
+            let specificationTags =
+                specification
+                |> Array.map fst
+                |> String.concat ", "
+
+            Decode.fail
+                (sprintf
+                    "Found tag does not match any in specification, expecting one of %A, got: %s"
+                     specificationTags
+                     foundTag)
+
+    decodeTypeTag tagField
+    |> Decode.andThen applyMatchingDecoder
